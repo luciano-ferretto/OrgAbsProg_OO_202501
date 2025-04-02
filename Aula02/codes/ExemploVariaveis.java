dac334c370a84f37bd83943bc59256751971fa5e
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2025-04-02 18:58:16 -0300

    Aula 02 - Final

---
+++ Aula02/codes/ExemploVariaveis.java
@@ -0,0 +1,9 @@
+public class ExemploVariaveis {
+    public static void main(String[] args) {
+        double var1;
+        double var2 = 2;
+        var1 = 1;
+        var2 = var1 / var2;
+        System.out.println("O valor do var2: " + var2);
+    }
+}
