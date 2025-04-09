bdb2db93f41df04f2ab479a56b9b2958ea8f908c
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2025-04-09 16:17:12 -0300

    Códigos feito em aula

---
+++ Aula05/Codes/FeitosEmAula/ExemploForTradicional.java
@@ -0,0 +1,8 @@
+public class ExemploForTradicional {
+    public static void main(String[] args) {
+        for (int i = 100; i > 0; i-=5) {
+            double raiz = Math.sqrt(i);
+            System.out.println("A rais de " + i + " é: " + raiz);
+        }   
+    }
+}
---
+++ Aula05/Codes/FeitosEmAula/For.py
@@ -0,0 +1,3 @@
+import math
+for i in range(100,0,-5) :
+    print(f"A raiz de ${i} é ${math.sqrt(i)}")
