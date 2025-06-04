f2a2c995cf8e997012a6562fbf5d4a3cbcd3c3d0
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2025-06-04 20:52:04 -0300

    Api-Sample

--- api-sample/src/main/java/br/edu/atitus/api_sample/controllers/GreetingController.java
+++ api-sample/src/main/java/br/edu/atitus/api_sample/controllers/GreetingController.java
@@ -1,15 +1,22 @@
 package br.edu.atitus.api_sample.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 
-	@GetMapping
-	public String getGreeting() {
-		return "Hello World!";
+	@GetMapping("/{namePath}")
+	public String getGreeting(
+			@RequestParam(required = false) String name,
+			@PathVariable(required = false) String namePath) {
+		if (name == null)
+			name = namePath != null ? namePath : "World";
+		String returnGreeting = String.format("%s %s!", "Hello", name);
+		return returnGreeting;
 	}
 }
