7e58b0f4504d221ca3d0dd530f1a012447ba827b
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2025-06-16 21:56:41 -0300

    Autenticação + JWT

--- api-sample/src/main/java/br/edu/atitus/api_sample/configs/ConfigSecurity.java
+++ api-sample/src/main/java/br/edu/atitus/api_sample/configs/ConfigSecurity.java
@@ -7,19 +7,23 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import br.edu.atitus.api_sample.components.AuthTokenFilter;
 
 @Configuration
 public class ConfigSecurity {
 
 	@Bean
-	SecurityFilterChain getSecurity(HttpSecurity http) throws Exception {
+	SecurityFilterChain getSecurity(HttpSecurity http, AuthTokenFilter filter) throws Exception {
 		http.csrf(csrf -> csrf.disable())
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth -> auth
 					.requestMatchers("/ws**", "/ws/**").authenticated()
-					.anyRequest().permitAll());
+					.anyRequest().permitAll())
+			.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 			
 		return http.build();
 	}
--- api-sample/src/main/java/br/edu/atitus/api_sample/controllers/AuthController.java
+++ api-sample/src/main/java/br/edu/atitus/api_sample/controllers/AuthController.java
@@ -3,12 +3,17 @@ package br.edu.atitus.api_sample.controllers;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.atitus.api_sample.components.JWTUtils;
+import br.edu.atitus.api_sample.dtos.SigninDTO;
 import br.edu.atitus.api_sample.dtos.SignupDTO;
 import br.edu.atitus.api_sample.entities.UserEntity;
 import br.edu.atitus.api_sample.entities.UserType;
@@ -19,12 +24,31 @@ import br.edu.atitus.api_sample.services.UserService;
 public class AuthController {
 	
 	private final UserService service;
+	private final AuthenticationConfiguration authConfig;
 	
 	// Injeção de dependência via método construtor
-	public AuthController(UserService service) {
+	public AuthController(UserService service, AuthenticationConfiguration authConfig) {
 		super();
 		this.service = service;
+		this.authConfig = authConfig;
 	}
+	
+	@PostMapping("/signin")
+	public ResponseEntity<String> signin(@RequestBody SigninDTO signin) {
+		try {
+			authConfig.getAuthenticationManager()
+				.authenticate(new UsernamePasswordAuthenticationToken(signin.email(), signin.password()));
+		} catch (AuthenticationException e) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+
+		} catch (Exception e) {
+			
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+		
+		return ResponseEntity.ok(JWTUtils.generateToken(signin.email()));
+	}
+	
 
 	@PostMapping("/signup")
 	public ResponseEntity<UserEntity> signup(@RequestBody SignupDTO dto) throws Exception{
